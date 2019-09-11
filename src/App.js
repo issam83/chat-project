070206513a237a4526ae6fb8f708972ea56b090e
Kamilia El agrebi <elagrebiissam@gmail.com>
2019-09-11 19:04:27 +0200

    gestion events

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import Message from './component/Message';
 import './App.css';
 
 class App extends Component {
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage = message => {
+    const messages = { ... this.state.messages}
+    messages[`message-${Date.now()}`] = message
+    this.setState({ messages })
+  }
+
   render() {
     return (
       <div className="box">
         <div className='messages'>
           <Message />
+          <Message />
+          <Message />
         </div>
-        <Formulaire />
+        <Formulaire
+        pseudo={this.state.pseudo}
+        addMessage={this.addMessage}/>
       </div>
     );
   }
